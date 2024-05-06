7d1e5ff41103aabcec8a13b3b99598387474bcce
vaurnadhityagb <varunadhitya.balaji@gmail.com>
2024-05-06 09:34:36 +0530

    Made a test branch to deploy the app locally

--- api/index.py
+++ api/index.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Hello, World!'
+    if request.method == 'POST':
+        data = request.get_json()
+        return "POst"
+    else:
+        return 'Hello, World!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == '__main__':
+    app.run(debug=True)
